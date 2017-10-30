6893b9045992b7715d977805e45f12e2b697ea75
Mohammed Abdulkereem <HorizonDragon@outlook.com>
2017-10-30 18:58:03 +0300

    Updated index to export the FrappeGantt component and it's dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,6 @@
-// Demo component
-// this is only example component
-
 import * as React from 'react';
-import { App } from './app';
+import { FrappeGantt, Task, ViewMode, FrappeGanttProps } from './FrappeGantt';
 
-class MyComponent extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    componentDidMount() {
-
-    }
-
-    render() {
-        return (
-            <div className="my-component">
-                <App />
-            </div>
-        )
-    }
-}
-
-export default MyComponent;
+export {
+    FrappeGantt, Task, ViewMode
+};
