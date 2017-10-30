335fa3b54b0fcc6255094caafe614ce28643c2cd
Mohammed Abdulkereem <HorizonDragon@outlook.com>
2017-10-30 18:58:24 +0300

    Updated Example to use FrappeGantt

--- examples/Example.tsx
+++ examples/Example.tsx
@@ -1,6 +1,33 @@
 import * as React from 'react';
 
-import MyComponent from '../src/index';
+import { FrappeGantt, Task } from '../src/index';
+
+const tasks = [
+    {
+        id: 'Task 1',
+        name: 'Redesign website',
+        start: '2016-12-28',
+        end: '2016-12-31',
+        progress: 20,
+        dependencies: ''
+    },
+    {
+        id: 'Task 2',
+        name: 'Redesign website',
+        start: '2016-12-28',
+        end: '2016-12-31',
+        progress: 20,
+        dependencies: 'Task 1'
+    },
+    {
+        id: 'Task 3',
+        name: 'Redesign website',
+        start: '2016-12-28',
+        end: '2016-12-31',
+        progress: 20,
+        dependencies: 'Task 2, Task 1'
+    }
+]
 
 class Example extends React.Component {
     constructor(props) {
@@ -14,7 +41,7 @@ class Example extends React.Component {
     render() {
         return (
             <div>
-                <MyComponent />
+                <FrappeGantt tasks={tasks} />
             </div>
         )
     }
